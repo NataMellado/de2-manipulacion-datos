e862e0577780b352fdda6b58b5c2d9d9afd75a33
Natalia Mellado <nataliamelladocani@gmail.com>
2024-03-11 13:00:43 -0300

    Update sql file

--- desafio2-natalia-mellado.sql
+++ desafio2-natalia-mellado.sql
@@ -1,62 +1,49 @@
+CREATE DATABASE desafio2-natalia-mellado;
+
 CREATE TABLE INSCRITOS(
     cantidad INT, 
     fecha DATE, 
     fuente VARCHAR(50)
 );
 
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 44, '01/01/2021', 'Blog' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 56, '01/01/2021', 'Página' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 39, '01/02/2021', 'Blog' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 81, '01/02/2021', 'Página' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 12, '01/03/2021', 'Blog' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 91, '01/03/2021', 'Página' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 48, '01/04/2021', 'Blog' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 45, '01/04/2021', 'Página' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 55, '01/05/2021', 'Blog' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 33, '01/05/2021', 'Página' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 18, '01/06/2021', 'Blog' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 12, '01/06/2021', 'Página' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 34, '01/07/2021', 'Blog' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 24, '01/07/2021', 'Página' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 83, '01/08/2021', 'Blog' );
-INSERT INTO INSCRITOS(cantidad, fecha, fuente)
-VALUES ( 99, '01/08/2021', 'Página' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 44, '01/01/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 56, '01/01/2021', 'Pï¿½gina' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 39, '01/02/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 81, '01/02/2021', 'Pï¿½gina' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 12, '01/03/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 91, '01/03/2021', 'Pï¿½gina' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 48, '01/04/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 45, '01/04/2021', 'Pï¿½gina' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 55, '01/05/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 33, '01/05/2021', 'Pï¿½gina' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 18, '01/06/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 12, '01/06/2021', 'Pï¿½gina' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 34, '01/07/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 24, '01/07/2021', 'Pï¿½gina' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 83, '01/08/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente) VALUES ( 99, '01/08/2021', 'Pï¿½gina' );
 
 select * from INSCRITOS;
 
--- ¿Cuántos registros hay?
+-- Â¿CuÃ¡ntos registros hay?
 SELECT COUNT(*) AS "Cantidad de registros"
 from INSCRITOS;
 
--- ¿Cuántos inscritos hay en total?
+-- Â¿CuÃ¡ntos inscritos hay en total?
 SELECT SUM(cantidad) AS "Total de inscritos"
 from INSCRITOS;
 
--- ¿Cuál o cuáles son los registros de mayor antigüedad?
+-- Â¿CuÃ¡l o cuÃ¡les son los registros de mayor antiguedad?
 SELECT * FROM INSCRITOS
 WHERE fecha = (SELECT MIN(fecha) FROM INSCRITOS);
 
--- ¿Cuántos inscritos hay por día?
-SELECT fecha, SUM(cantidad) AS "Inscritos por día"
+-- Â¿CuÃ¡ntos inscritos hay por dÃ¡a?
+SELECT fecha, SUM(cantidad) AS "Inscritos por dÃ­a"
 from INSCRITOS
 GROUP BY fecha;
 
--- ¿Qué día se inscribieron la mayor cantidad de personas y cuántas personas se inscribieron en ese día?
+-- Â¿QuÃ© dÃ­a se inscribieron la mayor cantidad de personas y 
+-- cuÃ¡ntas personas se inscribieron en ese dÃ­a?
 SELECT fecha, SUM(cantidad) as "Total de inscritos"
 from INSCRITOS
 GROUP BY fecha
